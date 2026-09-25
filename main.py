be369652176ba8bc707620d4d99badbe74e2c66b
Mr-Tetin <raimundo.porto5566@gmail.com>
2026-09-24 22:00:05 -0300

    Rotas adicionadas (ainda em desenvolvimento), e atualização de pacotes tirados e adicionados no requirements

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+#Rota da página incial
 @app.get("/")
 def paginaInicial():
     return "MovieTracker"
+
+#Rota para ver os filmes existentes
+@app.get("/filmes")
+def verFilmes():
+    ...
+
+@app.post("/adicionarFilme")
+def adicionarFilmes():
+    ...
